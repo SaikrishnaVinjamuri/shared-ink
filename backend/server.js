8b13eb816e797f6cff57cd60d52f4b5d07a2fa89
SaikrishnaVinjamuri <saikrishna17902@gmail.com>
2026-03-16 12:21:23 -0700

    update code

--- backend/server.js
+++ backend/server.js
@@ -7,12 +7,14 @@ const authRoute = require('./routes/authRoute')
 const blogRoutes = require('./routes/blogRoutes')
 const userRoutes = require('./routes/userRoutes')
 const cors = require("cors");
+const path = require('path');
 
 dotenv.config()
 dbConnect()
 
 const app = express()
 const port = process.env.PORT || 3000
+const _dirname = path.resolve();
 
 app.use(express.json())
 app.use(bodyParser.urlencoded({extended: true}))
@@ -28,6 +30,11 @@ app.use(
 app.use('/api/auth',authRoute)
 app.use('/api/blogs', blogRoutes)
 app.use('/api/users', userRoutes)
+
+app.use(express.static(path.join(_dirname, '/frontend/dist')));
+app.use((req, res) => {
+  res.sendFile(path.join(_dirname, '/frontend/dist/index.html'));
+});
 
 app.listen(port, ()=>{
     console.log(`server is running port ${port}`)
